8b3d5ea887daf9b093a0e92657aa458d436c30cd
NOMURA Ryu <ryunomura.3801.uni@gmail.com>
2023-12-02 05:42:09 +0900

    Make two tutorials Fusion102 & Fusion103

---
+++ Assets/Project/Fusion102/NetworkInputData.cs
@@ -0,0 +1,10 @@
+using Fusion;
+using UnityEngine;
+
+namespace No6RoN.Sandbox.Fusion102
+{
+    public struct NetworkInputData : INetworkInput
+    {
+        public Vector3 direction;
+    }
+}
---
+++ Assets/Project/Fusion102/Player.cs
@@ -0,0 +1,23 @@
+using Fusion;
+
+namespace No6RoN.Sandbox.Fusion102
+{
+    public class Player : NetworkBehaviour
+    {
+        private NetworkCharacterControllerPrototype _cc;
+
+        private void Awake()
+        {
+            _cc = GetComponent<NetworkCharacterControllerPrototype>();
+        }
+
+        public override void FixedUpdateNetwork()
+        {
+            if (GetInput(out NetworkInputData data))
+            {
+                data.direction.Normalize();
+                _cc.Move(5 * data.direction * Runner.DeltaTime);
+            }
+        }
+    }
+}
